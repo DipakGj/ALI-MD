531695fb7bc49db61b6559fd7268baf8ee1cea02
DipakGj <129220037+DipakGj@users.noreply.github.com>
2025-05-31 20:46:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "STARK-ALI~rlxg2JzR#tEIqTk5zKdpFU-brf4zoIL8opU_ePbBwCBk-nGn3eKI",
+SESSION_ID: process.env.SESSION_ID || "STARK-ALI~C9AhTJZa#ARKzESRnaAgrK5v8TvrkT4ba7bwKN3Wf5x4SjPsrWLY",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -17,7 +17,7 @@ AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "**",
 // set the auto reply massage on status reply  
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "ALI-MD",
+BOT_NAME: process.env.BOT_NAME || "Alone Babai",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "ALONE-BABAI",
 // type sticker pack name 
@@ -25,7 +25,7 @@ CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
+DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "917439857837",
 // add your bot owner number
@@ -33,7 +33,7 @@ OWNER_NAME: process.env.OWNER_NAME || "ALONE-BABAI",
 // add bot owner name 
 WELCOME: process.env.WELCOME || "falsee",
 // true if want welcome msg in groups
-GOODBYE: process.env.TUPODMARAGUDMARANI || "false",
+GOODBYE তুইপোদমারাবোকাচোদা: process.env.TUPODMARAGUDMARANI || "true",
 // true if want goodbye msg in groups
 ADMIN_STATUS: process.env.ADMIN_STATUS || "false",
 // make true to know who dismiss or promoted a member in group
